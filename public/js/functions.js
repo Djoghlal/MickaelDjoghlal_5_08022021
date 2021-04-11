0b0a5ca25c7f6a705d45f999bc50470c938ae6bb
Djoghlal Micka <pro.djoghlal@gmail.com>
2021-04-11 18:09:17 +0200

    2021-04-11 (V1)

--- public/js/functions.js
+++ public/js/functions.js
@@ -4,25 +4,60 @@ const eventContainer = document.querySelector('#event-status');
 
 
 function addPanier(idArticle ,optionArticle, quantityArticle) {
+    //On vérifie si le panier est existant ou pas pour la suite des étapes.
+    if (localStorage.getItem("basketTeddies")) {
+        //Le panier existe, on parcours le localStorage et vérifier à chaque boucle si l'ID et l'option existe.
+        let basketContainerJson = localStorage.getItem("basketTeddies");
+        let basketContainer = JSON.parse(basketContainerJson);
 
-    console.log(idArticle);
-    console.log(optionArticle);
-    console.log(quantityArticle);
-
-    // //On vérifie si le panier est existant ou pas pour la suite des étapes.
-    // if (localStorage.getItem("basketTeddies")) {
-    //     //Le panier existe donc pas besoin de le créer.
+        let teddieChoice = {
+            id: idArticle,
+            option: optionArticle,
+            quantity: parseInt(quantityArticle)
+        };
         
-    // } else {
-    //     //Le panier existe pas, nous pouvons créer le tableau et enregistrer directement l'objet dans le localStorage
-    //     let basketTeddies = [{
-    //         id: data._id,
-    //         name: data.name,
-    //         price: data.price/100,
-    //         //quantity: 
-    //         option: optionChoice
-    //     }];
+        for (let eltBasket of basketContainer) {
+            //Si id et option existe, on ajoute quantité
+            //Sinon, on ajoute l'objet complet
+            if (teddieChoice.id == eltBasket.id && teddieChoice.option == eltBasket.option) {
+                    console.log('Cet objet est déjà dans votre panier');
+                    let quantityFinaly = parseInt(eltBasket.quantity) + teddieChoice.quantity;
+                break;
+            } else {
+                    console.log(`L'objet n'est pas existant dans le panier !`)
+                    basketContainer.push(teddieChoice);
+                    basketContainerJson = JSON.stringify(basketContainer);
+                    localStorage.setItem("basketTeddies", basketContainerJson);
 
-    //     console.log(basketTeddies);
-    // }
+                    //On valide le message d'ajout au panier.
+                    eventContainer.innerHTML = `
+                    <h4 class="alert-heading">Validation</h4>
+                    <p>Votre article a été correctement ajouté au panier.</p>
+                    `;
+                    eventContainer.classList.replace('warning-none', 'success-view');
+                    eventContainer.classList.replace('alert-danger', 'alert-success');
+                break;
+            }
+        }
+        
+    } else {
+        //Le panier existe pas, nous pouvons créer le tableau et enregistrer directement l'objet dans le localStorage
+        let basketTeddieChoice = [{
+            id: idArticle,
+            option: optionArticle,
+            quantity: quantityArticle
+        }];
+
+        //On l'enregistre dans le localStorage
+        teddieChoiceJson = JSON.stringify(basketTeddieChoice);
+        localStorage.setItem("basketTeddies", teddieChoiceJson);
+
+        //On valide le message d'ajout au panier.
+        eventContainer.innerHTML = `
+            <h4 class="alert-heading">Validation</h4>
+            <p>Votre article a été correctement ajouté au panier.</p>
+        `;
+        eventContainer.classList.replace('warning-none', 'success-view');
+        eventContainer.classList.replace('alert-danger', 'alert-success');
+    }
 }
