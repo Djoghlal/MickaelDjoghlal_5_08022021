4a2693be184bc53a5e240d2c5102d9bc019a5564
Djoghlal Micka <pro.djoghlal@gmail.com>
2021-04-22 22:12:17 +0200

    2021-04-22 (V2)

--- public/js/panier.js
+++ public/js/panier.js
@@ -1,13 +1,12 @@
 //On récupère le contenu du localStorage
 const storageBasketJson = window.localStorage.getItem('basketTeddies');
-let resultBasketContainer = '';
-let arrayBasketFinaly = [];
 
 //Si le localStorage n'est pas vide, on traite la liste du contenu.
 //Sinon on dit au visiteur que son panier est vide.
 if (storageBasketJson != null) {
     //On récupère le contenu du localStorage
     let storageBasket = JSON.parse(storageBasketJson);
+    document.querySelector('#panier-container').innerHTML = '';
 
     for (const elt of storageBasket) { 
         let urlProduct = url + elt.id;
@@ -15,17 +14,22 @@ if (storageBasketJson != null) {
         fetch(urlProduct) .then(function(response) {
             return response.json();
         }) .then(function(dataBasket) {
-
-            //On affiche la ligne correspondante au tableau des résultats
-            dataBasketObject = {
-                id: elt.id,
-                name: dataBasket.name,
-                quantity: elt.quantity,
-                option: elt.option,
-                price: dataBasket.price
-            };
-
-            arrayBasketFinaly.push(dataBasketObject);
+                document.querySelector('#panier-container').innerHTML += `
+                    <div class="card card-basket" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Orico ${dataBasket.name}</h5>
+                            <p class="card-text"><span class="badge badge-success badge-price">${dataBasket.price/100}€</span></p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><strong>Quantité: </strong>${elt.quantity}</li>
+                            <li class="list-group-item"><strong>Option: </strong>${elt.option}</li>
+                            <li class="list-group-item"><strong>Prix total: </strong>${elt.quantity * dataBasket.price/100}€</li>
+                        </ul>
+                        <div class="card-body">
+                            <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+                        </div>
+                    </div>
+                `;
         })
 
         .catch(error => {
@@ -33,52 +37,10 @@ if (storageBasketJson != null) {
         });
     }
 
-    console.log(arrayBasketFinaly[1]);
-
-    // resultBasketContainer = `
-    //     <table class="table">
-    //         <thead class="thead-light">
-    //             <tr>
-    //                 <th scope="col">#</th>
-    //                 <th scope="col">Produit</th>
-    //                 <th scope="col">Prix</th>
-    //                 <th scope="col">Quantité</th>
-    //                 <th scope="col">Option</th>
-    //                 <th scope="col">Action</th>
-    //             </tr>
-    //         </thead>
-    //         <tbody>`;
-
-    // for (i=0; i < arrayBasketFinaly.length; i++) {
-    //     console.log(arrayBasketFinaly);
-    // }
-
-    // resultBasketContainer += `
-    //         </tbody>
-    //     </table>`;
-
 } else {
-    resultBasketContainer = `
-        <table class="table">
-            <thead class="thead-light">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nom du produit</th>
-                    <th scope="col">Prix</th>
-                    <th scope="col">Quantité</th>
-                    <th scope="col">Option</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">0</th>
-                    <td colspan="5">Votre panier est vide !</td>
-                </tr>
-            </tbody>
-        </table>
-    `;
+    document.querySelector('#panier-container').innerHTML += `
+    <div class="container-fluid">
+        <img alt="None" src="../images/ciao.gif" />
+        <p class="text-center lead">Votre panier est vide :'(</p>
+    </div>`;
 }
-
-//On insère le tableau dans le container de la page
-let panierContainer = document.querySelector('#panier-container').innerHTML = resultBasketContainer;
